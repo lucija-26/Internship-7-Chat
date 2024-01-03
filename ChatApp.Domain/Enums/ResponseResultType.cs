8aff6f3737de0c6237f9740ab593d5b074018207
lucija-26 <Lucija.Jurkovic.02@fesb.hr>
2024-01-04 00:39:27 +0100

    made enums in domain layer

---
+++ ChatApp.Domain/Enums/ResponseResultType.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace ChatApp.Domain.Enums
+{
+    public enum ResponseResultType
+    {
+        Success,
+        NotFound,
+        AlreadyExists,
+        NoChanges,
+        ValidationError
+    }
+}
