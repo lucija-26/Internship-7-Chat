9250ebcc8cc646fde3be5a0c26c279761b51e8a4
lucija-26 <Lucija.Jurkovic.02@fesb.hr>
2024-01-04 01:08:23 +0100

    made private messages repo

---
+++ ChatApp.Domain/Repositories/PrivateMessageRepository.cs
@@ -0,0 +1,34 @@
+﻿using ChatApp.Data.Entities.Models;
+using ChatApp.Data.Entities;
+using ChatApp.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Domain.Repositories
+{
+    public class PrivateMessageRepository : BaseRepository
+    {
+        public PrivateMessageRepository(ChatAppDbContext dbContext) : base(dbContext)
+        {
+        }
+        public ResponseResultType Add(PrivateMessage DirectMessage)
+        {
+            DbContext.PrivateMessages.Add(DirectMessage);
+
+            return SaveChanges();
+        }
+        public ResponseResultType DeletePrivateMessage(int messageId)
+        {
+            var messageToDelete = DbContext.PrivateMessages.Find(messageId);
+
+            if (messageToDelete == null)
+            {
+                return ResponseResultType.NotFound;
+            }
+
+            DbContext.PrivateMessages.Remove(messageToDelete);
+
+            return SaveChanges();
+        }
+
+    }
+}
