9fca8c9038eaaafdac994a376faa0a81ea0f6cab
lucija-26 <Lucija.Jurkovic.02@fesb.hr>
2024-01-04 21:43:43 +0100

    private message action

---
+++ ChatApp.Presentation/Actions/ExitMenuAction.cs
@@ -0,0 +1,19 @@
+﻿
+using ChatApp.Presentation.Abstractions;
+
+namespace ChatApp.Presentation.Actions
+{
+    public class ExitMenuAction : IAction
+    {
+        public int MenuIndex { get; set; }
+        public string Name { get; set; } = "Exit";
+
+        public ExitMenuAction()
+        {
+        }
+
+        public void Open()
+        {
+        }
+    }
+}
---
+++ ChatApp.Presentation/Actions/PrivateMessageAction.cs
@@ -0,0 +1,21 @@
+﻿using ChatApp.Presentation.Abstractions;
+using System.Xml.Linq;
+
+namespace ChatApp.Presentation.Actions
+{
+    public class PrivateMessageAction : BaseMenuAction
+    {
+        public PrivateMessageAction(IList<IAction> actions) : base(actions)
+        {
+            Name = "Private Messages menu";
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("Private Messages menu");
+            base.Open();
+        }
+
+    }
+
+}
