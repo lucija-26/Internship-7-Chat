f3c838f7bf76f8d371363f2c64ecc69721f53d7a
lucija-26 <Lucija.Jurkovic.02@fesb.hr>
2024-01-04 21:33:32 +0100

    added login action

---
+++ ChatApp.Presentation/Actions/LoginAction.cs
@@ -0,0 +1,18 @@
+﻿using ChatApp.Presentation.Abstractions;
+
+namespace ChatApp.Presentation.Actions
+{
+    public class LoginAction : BaseMenuAction
+    {
+        public LoginAction(IList<IAction> actions) : base(actions)
+        {
+            Name = "Login menu";
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("Login menu");
+            base.Open();
+        }
+    }
+}
