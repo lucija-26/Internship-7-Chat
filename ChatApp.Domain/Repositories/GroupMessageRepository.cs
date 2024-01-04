20166620e118723cd19370291a7e208ef913ad64
lucija-26 <Lucija.Jurkovic.02@fesb.hr>
2024-01-04 19:32:00 +0100

    added group message repository

---
+++ ChatApp.Domain/Repositories/GroupMessageRepository.cs
@@ -0,0 +1,37 @@
+﻿using ChatApp.Data.Entities;
+using ChatApp.Data.Entities.Models;
+using ChatApp.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Domain.Repositories
+{
+    public class GroupMessageRepository : BaseRepository
+    {
+        public GroupMessageRepository(ChatAppDbContext dbContext) : base(dbContext) { }
+
+        public ResponseResultType Add(GroupMessage GroupMessage)
+        {
+            DbContext.GroupMessages.Add(GroupMessage);
+
+            return SaveChanges();
+        }
+
+        public ICollection<GroupMessage> GetAll() => DbContext.GroupMessages.ToList();
+        public ResponseResultType Delete(int id)
+        {
+            var MessageToDelete = DbContext.GroupMessages.Find(id);
+            if (MessageToDelete is null)
+            {
+                return ResponseResultType.NotFound;
+            }
+
+            DbContext.GroupMessages.Remove(MessageToDelete);
+
+            return SaveChanges();
+        }
+    }
+}
